8a8c9318e748152d27455c8e6dc4f8e87e233f2e
yarinvak <yarinvak@gmail.com>
2019-03-04 01:39:12 +0200

    calling lima and getting severity

--- src/bl/connectors/LimaConnector.ts
+++ src/bl/connectors/LimaConnector.ts
@@ -1,11 +1,12 @@
-import axios, {AxiosPromise} from "axios";
+import axios from "axios";
 
 export class LimaConnector {
     url: string = "http://129.213.103.20:3000";
 
     getCrowdedPlaces(coords: { lat: number, long: number }) {
-        axios.get(this.url + '/crowdedPlaces?lat=' + coords.lat + '&lng=' + coords.long).then((response: any) => {
-            console.log("success call Lima - Crowded Places");
+        axios.get(this.url + '/crowdedPlaces?lat="' + coords.lat + '"&lng="' + coords.long + '"&triggeringPlacesTypes=' + 'school,' + 'zoo').then((response: any) => {
+            console.log("success calling Lima - Crowded Places");
+            console.log(response.data);
         }).catch((err) => console.log(err.response));
     }
 
