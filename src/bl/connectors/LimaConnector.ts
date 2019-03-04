aa4a8de298b4aa830e506bffc637ce9d275405d8
yarinvak <yarinvak@gmail.com>
2019-03-04 04:05:39 +0200

    get places from lima

--- src/bl/connectors/LimaConnector.ts
+++ src/bl/connectors/LimaConnector.ts
@@ -7,11 +7,16 @@ export class LimaConnector {
         axios.get(this.url + '/crowdedPlaces?lat="' + coords.lat + '"&lng="' + coords.long + '"&triggeringPlacesTypes=' + 'school,' + 'zoo').then((response: any) => {
             console.log("success calling Lima - Crowded Places");
             console.log(response.data);
-        }).catch((err) => {throw new Error(err)});
+        }).catch((err) => {
+            throw new Error(err)
+        });
     }
 
     getWeatherPreferences() {
-        console.log("gettinggg");
         return axios.get(this.url + '/weatherPreferences');
+    }
+
+    getPlaceTypes() {
+        return axios.get(this.url + "/placesTypes");
     }
 }
