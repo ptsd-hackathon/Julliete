2e2670e62e741943edd0ec64bf62b8a08a5ab5fc
yarinvak <yarinvak@gmail.com>
2019-03-04 07:31:25 +0200

    successfull fetch chen

--- src/bl/connectors/LimaConnector.ts
+++ src/bl/connectors/LimaConnector.ts
@@ -4,11 +4,11 @@ export class LimaConnector {
     url: string = "http://129.213.103.20:3000";
 
     getCrowdedPlaces(coords: { lat: number, long: number }, places: (string | null)[] | undefined) {
-        places=["store"];
+        places = ["airport", "political"];
         let placesString = places ? places.join() : "";
         console.log(placesString);
-        return axios.get(this.url + '/crowdedPlaces?lat="' + coords.lat + '"&lng="' + coords.long +
-            '"&triggeringPlacesTypes=' + placesString);
+        return axios.get(this.url + '/crowdedPlaces?lat=' + coords.lat + '&lon=' + coords.long +
+            '&triggeringPlacesTypes=' + placesString);
     }
 
     getWeatherPreferences() {
