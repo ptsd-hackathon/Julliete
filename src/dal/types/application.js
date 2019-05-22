0d9e2165c8e8c7e57b2b7eec0cd359c913f1cf77
yarinvak <yarinvak@gmail.com>
2019-05-22 22:23:59 +0300

    remove files!!

--- src/dal/types/application.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var AppSchema = new mongoose_1.Schema({
-    appName: String
-});
-exports.App = mongoose_1.model("App", AppSchema);
-//# sourceMappingURL=application.js.map
